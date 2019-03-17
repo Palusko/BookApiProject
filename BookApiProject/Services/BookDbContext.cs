1082ddcfa7e9a01f8e69fe78d6b2a4addb604758
Pavol <pavol@homerundesign.com>
2019-03-17 17:48:47 -0400

    Added DbContext

---
+++ BookApiProject/Services/BookDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BookApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApiProject.Services
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Review> Reviews { get; set; }
+        public virtual DbSet<Reviewer> Reviewers { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Country> Country { get; set; }
+        public virtual DbSet<BookAuthor> BookAuthors { get; set; }
+        public virtual DbSet<BookCategory> BookCategories { get; set; }
+    }
+}
