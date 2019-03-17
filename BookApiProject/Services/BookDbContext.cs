08594b02234aee13409b7cdacd41944ae069c634
Pavol <pavol@homerundesign.com>
2019-03-17 18:45:42 -0400

    AddingModelBuilder

--- BookApiProject/Services/BookDbContext.cs
+++ BookApiProject/Services/BookDbContext.cs
@@ -23,5 +23,30 @@ namespace BookApiProject.Services
         public virtual DbSet<Country> Country { get; set; }
         public virtual DbSet<BookAuthor> BookAuthors { get; set; }
         public virtual DbSet<BookCategory> BookCategories { get; set; }
+        
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookCategory>()
+                        .HasKey(bc => new { bc.BookId, bc.CategoryId });
+            modelBuilder.Entity<BookCategory>()
+                        .HasOne(b => b.Book)
+                        .WithMany(bc => bc.BookCategories)
+                        .HasForeignKey(b => b.BookId);
+            modelBuilder.Entity<BookCategory>()
+                        .HasOne(c => c.Category)
+                        .WithMany(bc => bc.BookCategories)
+                        .HasForeignKey(c => c.CategoryId);
+
+            modelBuilder.Entity<BookAuthor>()
+                        .HasKey(ba => new { ba.BookId, ba.AuthorId });
+            modelBuilder.Entity<BookAuthor>()
+                        .HasOne(a => a.Author)
+                        .WithMany(ba => ba.BookAuthors)
+                        .HasForeignKey(a => a.AuthorId);
+            modelBuilder.Entity<BookAuthor>()
+                        .HasOne(b => b.Book)
+                        .WithMany(ba => ba.BookAuthors)
+                        .HasForeignKey(b => b.BookId);
+        }
     }
 }
