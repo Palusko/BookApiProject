038ea349b7a576a23800b8f92594939f8f5a488d
Pavol <pavol@homerundesign.com>
2019-03-17 17:06:00 -0400

    Added Connection String

--- BookApiProject/Startup.cs
+++ BookApiProject/Startup.cs
@@ -5,17 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BookApiProject
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var connectionString = Configuration["connectionStrings:bookDbConnectionString"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
