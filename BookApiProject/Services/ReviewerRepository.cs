a2f961e9dcf441717ee2d04a9dd4e305c5bcc8dc
Pavol <pavol@homerundesign.com>
2019-03-24 21:21:42 -0400

    Implementing Reviewer Interface Repository

---
+++ BookApiProject/Services/ReviewerRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookApiProject.Models;
+
+namespace BookApiProject.Services
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private BookDbContext _reviewerContext;
+
+        public ReviewerRepository(BookDbContext reviewerContext)
+        {
+            _reviewerContext = reviewerContext;
+        }
+
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _reviewerContext.Reviewers.Where(r => r.Id == reviewerId).FirstOrDefault();
+        }
+
+        public Reviewer GetReviewerOfAReview(int reviewId)
+        {
+            var reviewerId = _reviewerContext.Reviews.Where(r => r.Id == reviewId).Select(rr => rr.Reviewer.Id).FirstOrDefault();
+            return _reviewerContext.Reviewers.Where(r => r.Id == reviewerId).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _reviewerContext.Reviewers.OrderBy(r => r.LastName).ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _reviewerContext.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _reviewerContext.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
