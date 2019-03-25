2798d97ee133cbe7acb549acfda73237bec41cc7
Pavol <pavol@homerundesign.com>
2019-03-24 22:42:11 -0400

    GetReviewers API action

---
+++ BookApiProject/Controllers/ReviewersController.cs
@@ -0,0 +1,47 @@
+﻿using BookApiProject.Dtos;
+using BookApiProject.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewersController : Controller
+    {
+        private IReviewerRepository _reviewerRepository;
+        public ReviewersController(IReviewerRepository reviewerRepository)
+        {
+            _reviewerRepository = reviewerRepository;
+        }
+
+        //api/reviewers
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ReviewerDto>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewers()
+        {
+            var reviewers = _reviewerRepository.GetReviewers();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewersDto = new List<ReviewerDto>();
+
+            foreach (var reviewer in reviewers)
+            {
+                reviewersDto.Add(new ReviewerDto
+                {
+                    Id = reviewer.Id,
+                    FirstName = reviewer.FirstName,
+                    LastName = reviewer.LastName
+                });
+            }
+
+            return Ok(reviewersDto);
+        }
+    }
+}
--- BookApiProject/Startup.cs
+++ BookApiProject/Startup.cs
@@ -31,6 +31,7 @@ namespace BookApiProject
 
             services.AddScoped<ICountryRepository, CountryRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IReviewerRepository, ReviewerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
