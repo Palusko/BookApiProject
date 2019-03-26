6a84337cf7063afbfd49ccd038c817d769af63b8
Pavol <pavol@homerundesign.com>
2019-03-25 22:59:18 -0400

    Implementing IReview Interface

---
+++ BookApiProject/Services/ReviewRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookApiProject.Models;
+
+namespace BookApiProject.Services
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private BookDbContext _reviewContext;
+
+        public ReviewRepository(BookDbContext reviewContext)
+        {
+            _reviewContext = reviewContext;
+        }
+
+        public Book GetBookOfAReview(int reviewId)
+        {
+            var bookId = _reviewContext.Reviews.Where(r => r.Id == reviewId).Select(b => b.Book.Id).FirstOrDefault();
+            return _reviewContext.Books.Where(b => b.Id == bookId).FirstOrDefault();
+        }
+
+        public Review GetReview(int reviewId)
+        {
+            return _reviewContext.Reviews.Where(r => r.Id == reviewId).FirstOrDefault();
+        }
+
+        public ICollection<Review> GetReviews()
+        {
+            return _reviewContext.Reviews.OrderBy(r => r.Rating).ToList();
+        }
+
+        public ICollection<Review> GetReviewsOfABook(int bookId)
+        {
+            return _reviewContext.Reviews.Where(b => b.Book.Id == bookId).ToList();
+        }
+
+        public bool ReviewExists(int reviewId)
+        {
+            return _reviewContext.Reviews.Any(r => r.Id == reviewId);
+        }
+    }
+}
--- BookApiProject/Startup.cs
+++ BookApiProject/Startup.cs
@@ -32,6 +32,7 @@ namespace BookApiProject
             services.AddScoped<ICountryRepository, CountryRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IReviewerRepository, ReviewerRepository>();
+            services.AddScoped<IReviewRepository, ReviewRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
